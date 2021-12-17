1b1d9bc336369eb40a233cfcdb50f4b3a038e160
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2021-12-17 15:09:25 +0100

    Added footer with credits to the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,13 @@ import { SVContext } from '../utils/SVContext';
 @Component({
 	selector: 'home-component',
 	templateUrl: './home.component.html',
-	host: { class: "pageComponent" }
+	host: { class: "pageComponent" },
+	styles: [`
+		.creditTitle { font-size: .875rem; font-weight: 700!important; }
+		.organizzationName { color: #002244; font-size: .875rem; font-weight:bold; } 
+		.organizzationSubtitle { color: #aa4400; font-size: .75rem; font-weight: bold; font-style: italic; }
+		.organizzationLogo { max-width: 60px; max-height: 36px; }
+	`],
 })
 export class HomeComponent implements OnInit {
 
